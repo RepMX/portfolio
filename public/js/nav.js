d2cdec23f0e9b9e7aaba50123dedab3fc03bb088
RepMX <78993814+RepMX@users.noreply.github.com>
2026-05-03 15:45:56 -0700

    Update nav.js
    Remove page-heading click listener

--- public/js/nav.js
+++ public/js/nav.js
@@ -18,7 +18,6 @@ function closeNav(delay = 180) {
 
 if (pageHeading && navOverlay) {
   pageHeading.addEventListener('pointerenter', openNav);
-  pageHeading.addEventListener('click', openNav);
 
   pageHeading.addEventListener('pointerleave', () => {
     closeNav(220);
