4423ad781f4647e9e53b80918786fed91dcf0f89
RepMX <78993814+RepMX@users.noreply.github.com>
2026-05-06 16:12:14 -0700

    Remove content loaded on site shell

--- public/js/site-shell.js
+++ public/js/site-shell.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.insertAdjacentHTML('beforeend', `
-    <div class="top-fade"></div>
-    <div class="bottom-fade"></div>
-    <div id="nav-overlay">
-        <div class="nav-backdrop"></div>
-        <div class="nav-panel">
-            <a href="/">home</a>
-            <a href="/filmworks/">filmworks</a>
-            <a href="/about/">about</a>
-            <a href="/contact/">contact</a>
-        </div>
+document.body.insertAdjacentHTML('beforeend', `
+<div class="top-fade"></div>
+<div class="bottom-fade"></div>
+<div id="nav-overlay">
+    <div class="nav-backdrop"></div>
+    <div class="nav-panel">
+        <a href="/">home</a>
+        <a href="/filmworks/">filmworks</a>
+        <a href="/about/">about</a>
+        <a href="/contact/">contact</a>
     </div>
-  `);
-});
+</div>
+`);
